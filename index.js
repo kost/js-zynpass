4c93d0dc7ccba956a8c6fd81533cd84b73952678
Vlatko Kosturjak <vlatko.kosturjak@gmail.com>
2017-03-10 04:30:06 +0100

    resolve test mixups

--- index.js
+++ index.js
@@ -1,33 +1,68 @@
-'use strict';
+(function() {
+  var Zynpass = (function() {
 
-var expect = require('chai').expect;
-var Zynpass = require('../index');
-var zynpass = new Zynpass();
+    var Zynpass = function() {
+    };
 
-// console.log(zynpass);
+    function zyncalc (seed, mac, magic) {
+	    var seed_str = seed.substring(0,6);
+	    var mac_num = parseInt(mac, 16) & 0x7;
+	    var seed_num = parseInt(seed_str, 16) & 0x00FFFFFF;
 
-describe('#zynPass', function() {
-    it('zynpass - should generate secret same as magic', function() {
-	var result = zynpass.zyngenpass('000000000000');
-	expect(result).to.equal('10F0A563');
-    });
-    it('zynpass - should generate secret for seed 000000000001', function() {
-	var result = zynpass.zyngenpass('000000000001');
-	expect(result).to.equal('887852B1');
-    });
-});
+	    var magicnum=UINT32(magic,16);
+	    // console.log(magicnum.toString(16));
+	    var seed_u32=UINT32(seed_num); 
 
-describe('#zynSecret', function() {
-    it('zynsecret - should generate secret same as magic', function() {
-	var result = zynpass.zynsecret('000000000000','00');
-	expect(result).to.equal('A11F5AC6');
-    });
+	    var calctmp_u32=seed_u32.clone().add(magicnum);
+	    // console.log(calctmp_u32.toString(16));
+	    calctmp_u32.rotr(mac_num); // rotate right by mac_num
+	    // console.log(calctmp_u32.toString(16));
+	    calctmp_u32.xor(seed_u32); // xor with seed
+	    // console.log(calctmp_u32.toString(16));
 
-    it('zynsecret - should generate secret from test number 100000000000 and 01', function() {
-	var result = zynpass.zynsecret('100000000000','01');
-	expect(result).to.equal('5087AD63');
-    });
+	    var ret = calctmp_u32.toString(16).toUpperCase();
+	    return ret;
+    }
+    Zynpass.prototype.zyncalc = zyncalc;
 
-});
+    function zyngenpass (seed) {
+	    var mac_str = seed.slice(-2); // use last two characters
+	    var ret = this.zyncalc (seed, mac_str, "10F0A563"); // 0x10F0A563
+	    return ret;
+    }
+    Zynpass.prototype.zyngenpass = zyngenpass;
+
+    function zynsecret (seed, mac) {
+	    var ret = this.zyncalc (seed, mac, "A11F5AC6"); // 0xA11F5AC6
+	    return ret;
+    }
+    Zynpass.prototype.zynsecret = zynsecret;
+
+    function zynaddaten (str) {
+	    var ret = 'ATEN1, ' + str;
+	    return ret;
+    }
+    Zynpass.prototype.zynaddaten=zynaddaten;
+
+    // console.log(Zynpass);
+    return Zynpass;
+  })();
+
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	    var UINT32 = require('cuint').UINT32;
+	    module.exports = Zynpass;
+	  }
+	  else {
+	    if (typeof define === 'function' && define.amd) {
+	      define([], function() {
+		return Zynpass;
+	      });
+	    }
+	    else {
+	      window.Zynpass = Zynpass;
+	    }
+	  }
+
+})();
 
 
